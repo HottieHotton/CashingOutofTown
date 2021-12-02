4d63c50e30c2c5b0d7e5a618a1eace428b4aa0da
Bhotton25 <bhotton25@gmail.com>
2021-12-01 19:35:02 -0700

    Added Basic HTML with group, and basic js

---
+++ assets/js/site.js
@@ -0,0 +1,14 @@
+//User enters location, have Google Maps pull data from said location
+var map= document.querySelector("submit");
+var locate = document.querySelector("locate");
+
+//Use other API to pull resturant reviews and display
+
+
+//User enters amount and selects tip rate, then display totals
+
+
+
+
+
+map.addEventListener("on-click", locate);
